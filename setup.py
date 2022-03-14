dccefa9423cbcc44d1c998cfb88b627c03fb6a1d
marcorossi5 <rssmrc.11@gmail.com>
2022-03-14 10:35:59 +0100

    adding requirements.txt and tf into setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,36 @@
+"""
+    Use the setup.py legacy version for package dependencies management.
+
+    The problem is related to tensorflow package:
+    when tensorflow-cpu or tensorflow-gpu are already installed, `pip` does not
+    recognize them and forces a re-install of tensorflow, which might not be the
+    best optimized package version for the current platform architecture
+
+    Solution:
+    all package metadata are handled statically by setup.cfg in PEP 517 style
+    package dependencies are handled dinamically by this module.
+
+"""
 from setuptools import setup
 
+requirements = [
+    "awkward",
+    "numpy",
+    "jupyterlab",
+    "matplotlib",
+    "scipy",
+    "sklearn",
+    "pyyaml",
+    "uproot",
+]
+
+try:
+    import tensorflow
+except ImportError:
+    requirements.append("tensorflow")    
+
+
 if __name__ == "__main__":
-    setup()
+    setup(
+        install_requires=requirements
+    )

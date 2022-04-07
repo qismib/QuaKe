d84a9ed5604fb25191f31b5335bb815b55d818f0
marcorossi5 <rssmrc.11@gmail.com>
2022-04-07 13:46:44 +0200

    update diagnostics visualization

--- src/quake/utils/diagnostics.py
+++ src/quake/utils/diagnostics.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
 from quake.utils.configflow import bool_me
+from sklearn.manifold import TSNE
 
 
 def histogram_activations_image(y_pred: tf.Tensor, y_true: tf.Tensor) -> plt.Figure:
@@ -63,9 +64,14 @@ def histogram_activations_image(y_pred: tf.Tensor, y_true: tf.Tensor) -> plt.Fig
 
 def scatterplot_features_image(features: tf.Tensor, y_true: tf.Tensor) -> plt.Figure:
     """
-    Returns a scatterplot of the first two features.
+    Returns a 2D scatterplot of the input features.
     Useful to look visually the separation in the 2D plane for binary
     classification.
+    The input features should have shape `(nb points, nb dims)`.
+    If `nb dims == 2`, plot features in 2D plane.
+    If `nb dims == 3`, plot features in 3D space.
+    If `nb dims > 3`, use tSNE technique to reduce point to 2 dimensions and
+    plot into 2D plane.
 
     Parameters
     ----------
@@ -77,17 +83,36 @@ def scatterplot_features_image(features: tf.Tensor, y_true: tf.Tensor) -> plt.Fi
         - the pyplot scatterplot
     """
     features = features.numpy()
-    standardize_fn = lambda x: (x - x.mean()) / x.std()
-    xs = standardize_fn(features[:, 0])
-    ys = standardize_fn(features[:, 1])
-
+    features = (features - features.mean(0, keepdims=True)) / features.std(0, keepdims=True)
+    nb_dims = features.shape[1]
+    
+    projection = '3d' if nb_dims == 3 else None # use 3D space only if nb_dims is 3
+    # define scatter function arguments depending on point dimensionality
+    if 1 < nb_dims <= 3:
+        pos = features[y_true]
+        neg = features[~y_true]
+        if nb_dims == 2:
+            pos_args = (pos[:,0], pos[:,1])
+            neg_args = (neg[:,0], neg[:,1])
+        if nb_dims == 3:
+            pos_args = (pos[:,0], pos[:,1], pos[:,2])
+            neg_args = (neg[:,0], neg[:,1], neg[:,2])
+    elif nb_dims > 3:
+        transformed = TSNE(n_components=2, learning_rate='auto', init='random').fit_transform(features)
+        pos = transformed[y_true]
+        neg = transformed[~y_true]
+        pos_args = (pos[:,0], pos[:,1])
+        neg_args = (neg[:,0], neg[:,1])
+    else:
+        raise ValueError(f"The extracted features must be more than 1, got {nb_dims}")
+    
     y_true = bool_me(y_true).numpy()
 
     figure = plt.figure()
-    ax = figure.add_subplot()
+    ax = figure.add_subplot(projection=projection)
     ax.title.set_text("Standardized network extracted features")
-    ax.scatter(xs[y_true], ys[y_true], c="green", s=10, label=r"$0\nu\beta\beta$")
-    ax.scatter(xs[~y_true], ys[~y_true], c="red", s=10, label=r"$e^-$")
+    ax.scatter(*pos_args, c="green", s=10, label=r"$0\nu\beta\beta$")
+    ax.scatter(*neg_args, c="red", s=10, label=r"$e^-$")
     ax.set_xlabel(r"$1^{st}$ feature $[\sigma_x]$")
     ax.set_ylabel(r"$2^{nd}$ feature $[\sigma_y]$")
     ax.legend()

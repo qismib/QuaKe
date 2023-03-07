5257c09e0b0dca5ab8e03f726b2653237bbce1b3
rmoretti9 <r.moretti9@campus.unimib.it>
2023-03-07 13:53:35 +0100

    batch normalization and example fixing

--- examples/cnn_scan.sh
+++ examples/cnn_scan.sh
@@ -1,8 +1,6 @@
-# min_en=("0.01" "0.05" "0.01")
-min_en=("0.2")
-
-res=($(seq 0.75 0.25 3.5))
-
+min_en=("0.01" "0.05" "0.1", "0.2")
+min_en=("0.01")
+res=($(seq 0.5 0.25 7.5))
 # res=("0.5")
 # res=($(seq 3.75 0.25 7.5))
 # res=("6.25")
@@ -12,8 +10,8 @@ for j in "${!min_en[@]}"; do
     printf "${min_en[$j]}"
     printf "${res[$i]}"
         quake datagen ../cards/runcard.yaml -o ../../output --force --res ${res[$i]} --energy ${min_en[$j]}
-        quake train -o ../../output -m blob --force
+        # quake train -o ../../output -m blob --force
         quake train -o ../../output -m attention --force
-        quake train -o ../../output -m cnn --force
+        # quake train -o ../../output -m cnn --force
     done
 done
